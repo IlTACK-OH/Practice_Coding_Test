ee6d075745151a220d409a3e4351a6f38ce0a8f7
ILTACK OH <113511013+IlTACK-OH@users.noreply.github.com>
2023-04-20 23:31:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    num_list = [int(i) for i in my_string if i.isdigit()]
-    answer = 0
-    for i in num_list:
-        answer += i
-    return answer
+    return sum([int(i) for i in my_string if i.isdigit()])
